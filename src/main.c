9c3723ca6cff9693147308a1b7086731d9fc43ad
Khachatryan-nn <tigrankh02@gmail.com>
2023-06-25 21:30:59 +0400

    [commit]

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tikhacha <tikhacha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 16:33:29 by musimony          #+#    #+#             */
-/*   Updated: 2023/06/25 21:28:47 by tikhacha         ###   ########.fr       */
+/*   Updated: 2023/06/25 21:30:45 by tikhacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ int	main(int ac, char **av, char **env1)
 			else if (ft_strcmp(str, "exit") == 0)
 			//{
 			//	system("leaks minishell");
-				exit(0);
+				exit (0);
 			//}
 			else if (ft_strcmp(str, "ls") == 0)
 			{
