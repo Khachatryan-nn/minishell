a2b7ec64f27185b3fbbeb9aa36bad56211658af6
Khachatryan-nn <tigrankh02@gmail.com>
2023-09-09 02:43:29 +0400

    Now unlinking .heredoc files that created before syntax error and was not deleted

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,14 +6,17 @@
 /*   By: tikhacha <tikhacha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 15:38:29 by tikhacha          #+#    #+#             */
-/*   Updated: 2023/09/09 02:25:42 by tikhacha         ###   ########.fr       */
+/*   Updated: 2023/09/09 02:41:59 by tikhacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		lexer(t_tok **res, char **line);
+int		handle_tokens_p1(t_tok **res, char **line, int *i, int counter);
+void	heredoc_validation(t_init *init, t_tok *tok);
 void	lex(char **line, t_init *init, t_env *env);
+int		lexer(t_tok **res, char **line);
+void	unlink_heredocs(t_init	*init);
 
 int	handle_tokens_p1(t_tok **res, char **line, int *i, int counter)
 {
@@ -93,6 +96,19 @@ void	heredoc_validation(t_init *init, t_tok *tok)
 	}
 }
 
+void	unlink_heredocs(t_init	*init)
+{
+	t_tok	*tok;
+
+	tok = init->lex;
+	while (tok)
+	{
+		if (tok->type == HEREDOC && tok->hdoc_fname)
+			unlink(tok->hdoc_fname);
+		tok = tok->next;
+	}
+}
+
 void	lex(char **line, t_init *init, t_env *env)
 {
 	t_tok	*tmp;
@@ -101,6 +117,7 @@ void	lex(char **line, t_init *init, t_env *env)
 	heredoc_validation(init, tmp);
 	if (!lexer(&init->lex, line) || !init->lex || !is_valid(init, env))
 	{
+		unlink_heredocs(init);
 		destroy_init(init);
 		init->exit_status = 258;
 		return ;
